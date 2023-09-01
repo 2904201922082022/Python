b315a95d45183724285ac2e32851a9f0e95c4037
Vitor Santos <126620601+2904201922082022@users.noreply.github.com>
2023-09-01 11:41:32 -0300

    Add files via upload

--- Automation_Tests_Cdd.py
+++ Automation_Tests_Cdd.py
@@ -19,3 +19,23 @@ def test_access_salsa():
     driver.save_screenshot('screenshot.png')
     driver.quit()
 
+#step 2
+#user input login field
+
+driver1 = webdriver.login_xpath
+driver1.find_element_by_xpath.login_xpath.send_keys.login_value
+webdriver.save_screenshot("step2.png")
+
+
+#step 3
+#user input password
+driver2 = webdriver.pwd_xpath
+driver2.find_element_by_xpath.pwd_xpath.send_keys.pwd_value
+webdriver.save_screenshot("step3.png")
+
+#step 4
+#user can access Salsa
+driver3 = webdriver.Continue_Btn
+driver3.find_element_by_xpath.Continue_Btn.click()
+webdriver.save_screenshot("step4.png")
+TimeoutError.sleep(2)
